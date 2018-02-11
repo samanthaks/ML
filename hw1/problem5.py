a104bee2e4e2d0c76830192c1eb11b69d8092a64
emilysong <eks2138@columbia.edu>
2018-02-11 15:00:05 -0500

    Multivariate Gaussian has accuracy of ~0.95 on test data set with 80/20 split

--- hw1/problem5.py
+++ hw1/problem5.py
@@ -1,51 +1,86 @@
 import scipy.io as spio
 import numpy as np
+from random import sample
 import math
 
-def read_file(filename):
-	'''
-	Returns the images matrix and labels matrix
-	'''
-	data = spio.loadmat(filename)
-	X = np.asarray(data['X'], dtype=np.int32)
-	Y = np.asarray(data['Y'], dtype=np.int32)
-	return X,Y
-
-class MultiGauss():
-	'''
-	Multivariate Gaussian Classifier
-	'''
-	def __init__(self):
+class ProbClassifier():
+	def __init__(self,filename,ratio):
 		'''
 		Initializes the data
 		'''
-		self.X,self.Y = read_file('hw1data.mat')
-		self.preprocess_data() 
-		self.separated_data = self.separate_by_label()
-		self.means,self.covs = self.get_mles()
+		self.X,self.Y = self.read_file(filename)
+		self.X_train,self.X_test,self.Y_train,self.Y_test = self.separate_data(ratio)
+		self.preprocess_data()
+
+	def read_file(self,filename):
+		'''
+		Returns the images matrix and the labels matrix
+		'''
+		data = spio.loadmat(filename)
+		X = np.asarray(data['X'], dtype=np.int32)
+		Y = np.asarray(data['Y'], dtype=np.int32)
+		return X,Y
+
+	def separate_data(self,ratio):
+		'''
+		Create training and testing sets
+		'''
+		inds = sample(range(len(self.X)),int(ratio*len(self.X)))
+		X_train = self.X[inds,]
+		X_test = np.delete(self.X,inds,axis=0)
+		Y_train = self.Y[inds]
+		Y_test = np.delete(self.Y,inds,axis=0)
+		return X_train,X_test,Y_train,Y_test
 
 	def preprocess_data(self):
 		'''
 		Preprocess data to avoid underflow or overflow
 		'''
-		# Choose the top 200 features with the highest variance
+		# Choose the top 200 features with the highest variance (choose features using training data but also apply this to testing data)
 		vars = []
-		for feature in self.X.T:
+		for feature in self.X_train.T:
 	   		vars.append(np.var(feature))
 		inds = np.argpartition(vars, -200)[-200:]
-		self.X = self.X[:,inds]
-		self.Y = self.Y[inds]
+		self.X_train = self.X_train[:,inds]
+		self.X_test = self.X_test[:,inds]
 
-		# Normalize each of the features to have mean zero and variance one.
-		self.X = (self.X - self.X.mean(axis=0)) / self.X.std(axis=0)
+		# Normalize each of the features to have mean zero and variance one (do this separately for training data and testing data)
+		self.X_train = (self.X_train - self.X_train.mean(axis=0)) / self.X_train.std(axis=0)
+		self.X_test = (self.X_test - self.X_test.mean(axis=0)) / self.X_test.std(axis=0)
+
+	def evaluate(self):
+		'''
+		Predicts on testing data, compares to true labels
+		'''
+		predictions = []
+		for image in self.X_test:
+			label = self.predict(image)
+			predictions.append(label)
+
+		count = 0
+		for pred,act in zip(predictions,self.Y_test):
+			if pred == act:
+				count += 1
+		return count/len(predictions)
+
+class MultiGauss(ProbClassifier):
+	'''
+	Multivariate Gaussian Classifier
+	'''
+	def __init__(self,filename,ratio):
+		'''
+		Initializes the data
+		'''
+		ProbClassifier.__init__(self,filename,ratio)
+		self.separated_data = self.separate_by_label()
+		self.means,self.covs = self.get_mles()
 
 	def separate_by_label(self):
 		'''
 		Creates list of lists, each containing the data for one label
 		'''
-		# Separate data by label
 		separated_data = [[] for i in range(10)] # images for the label that corresponds to that index, i.e. [[images for label 0],[images with label 1],...]
-		for vector,label in zip(self.X,self.Y):
+		for vector,label in zip(self.X_train,self.Y_train):
 			separated_data[int(label)].append(vector)
 		return separated_data
 
@@ -53,9 +88,8 @@ class MultiGauss():
 		'''
 		Creates list of means and covariance matrices, for all labels in the data
 		'''
-		# Get means, covs for each label
-		means = []
-		covs = []
+		means = [] # means for the label that corresponds to that index, i.e. [[mean image for label 0],[mean image for label 1],...]
+		covs = [] # covariance matrices for the label that corresponds to that index, i.e. [[cov for label 0],[cov for label 1],...]
 		offset = 0.1 * np.identity(200)
 		for label_data in self.separated_data:
 			label_data = np.asarray(label_data)
@@ -67,14 +101,14 @@ class MultiGauss():
 		'''
 		Calculates prior which will be used to determine probability of label
 		'''
-		prior = len(self.separated_data[label])/len(self.X)
+		prior = len(self.separated_data[label])/len(self.X_train)
 		return prior
 
 	def get_conditional(self,label,image):
 		'''
 		Calculates conditional using Multivariate Gaussian formula, used to determine probability of label
 		'''
-		frac = 1/np.linalg.det(self.covs[label])
+		frac = 1/math.sqrt(np.linalg.det(self.covs[label]))
 		diff = np.subtract(image,self.means[label])
 		inverse = np.linalg.inv(self.covs[label])
 		power = np.matmul(diff.T,np.matmul(inverse,diff))
@@ -95,32 +129,19 @@ class MultiGauss():
 			if probability > max_probability:
 				max_probability = probability
 				max_label = label
+		print(max_label)
 		return max_label
 
-	def evaluate(self):
-		'''
-		Predicts on testing data, compares to true labels
-		'''
-		predictions = []
-		for image in self.X[1:200,]:
-			label = self.predict(image)
-			predictions.append(label)
-
-		count = 0
-		for pred,act in zip(predictions,self.Y[1:200,]):
-			if pred == act:
-				count += 1
-		return count/len(predictions)
-
-class kNN():
+class kNN(ProbClassifier):
 	'''
 	k-Nearest Neighbor Classifier
 	'''
-	def __init__(self):
+	def __init__(self,filename,ratio):
 		'''
 		Initializes the data
 		'''
-		self.X,self.Y = read_file('hw1data.mat')
+		ProbClassifier.__init__(self,filename,ratio)
+		self.k = 1
 
 	def euclidean(self,image1,image2):
 		'''
@@ -128,39 +149,25 @@ class kNN():
 		'''
 		diff = np.subtract(image1,image2)
 		prod = np.matmul(diff.T,diff)
-		return math.pow(prod,0.5)
+		return math.sqrt(prod)
 
 	def predict(self,test_image):
 		'''
 		Predicts label given image
 		'''
-		min_dist = float('inf')
+		all_dist = []
 		label = -1
-		for (train_image,train_label) in zip(self.X,self.Y):
+		for train_image in self.X_train:
 			curr_dist = self.euclidean(test_image,train_image)
-			if curr_dist < min_dist:
-				min_dist = curr_dist
-				label = train_label
-		return label
-
-	def evaluate(self):
-		'''
-		Predicts on testing data, compares to true labels
-		'''
-		predictions = []
-		for image in self.X[1:200,]:
-			label = self.predict(image)
-			predictions.append(label)
-
-		count = 0
-		for pred,act in zip(predictions,self.Y[1:200,]):
-			if pred == act:
-				count += 1
-		return count/len(predictions)
+			all_dist.append(curr_dist)
+		inds = np.argpartition(curr_dist, -self.k)[-self.k:]
+		labels = self.Y_train[inds]
+		counts = np.bincount(labels[0])
+		return np.argmax(counts)
 		
 if __name__ == "__main__":
-	#mg_classifier = MultiGauss()
-	#print(mg_classifier.evaluate())
+	mg_classifier = MultiGauss('hw1data.mat',0.8)
+	print(mg_classifier.evaluate())
 
-	knn_classifier = kNN()
-	print(knn_classifier.evaluate())
+	#knn_classifier = kNN('hw1data.mat',0.8)
+	#print(knn_classifier.evaluate())
