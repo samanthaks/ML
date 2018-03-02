d39269e83554a03bb0c6bd68bbb3e7ae09509c77
emilysong <eks2138@columbia.edu>
2018-03-02 15:43:51 -0500

    Implement all pseudocode except for the gradient descent functions, currently working on adding that in

--- hw2/q6/2_layer_net.py
+++ hw2/q6/2_layer_net.py
@@ -1,88 +1,96 @@
-import pickle
 import scipy.io as spio
-import random
 import numpy as np
-
-
-data_file = 'hw2data_2.mat'
+import matplotlib.pyplot as plt
 
 class NeuralNet_2L():
     '''
         2 layer neural network for 1-d functions
     '''
 
-    def __init__(self, train_ratio):
+    def __init__(self,data_file,k):
         '''
-            Initialize
+            Initialize the data
         '''
-        # self.k = 4
-        self.X, self.Y = self.read_data()
-        self.train_ratio = train_ratio
-        self.X_train,self.X_test,self.Y_train,self.Y_test = self.separate_data()
-        self.weights = self.gradient_descent(1)
-        if self.train_ratio < 1:
-            self.accuracy = self.evaluate()
+        self.X,self.Y = self.read_data(data_file)
+        self.k = k
+        self.W1,self.b1,self.W2,self.b2 = self.gradient_descent()
 
-
-    def read_data(self, filename):
+    def read_data(self,data_file):
         '''
-            reads file
+            Gets x and y from file
         '''
         data = spio.loadmat(data_file)
         X = np.asarray(data['X'], dtype=np.int32)
         Y = np.asarray(data['Y'], dtype=np.int32)
-        print('Finished reading in the file')
+        print('Finished getting X and Y from the file')
         return X,Y
 
-    def separate_data(self):
-        '''
-            Create training and testing sets
-        '''
-        inds = sample(range(len(self.X)),int(self.train_ratio*len(self.X)))
-        X_train = self.X[inds,]
-        X_test = np.delete(self.X,inds,axis=0)
-        Y_train = self.Y[inds]
-        Y_test = np.delete(self.Y,inds,axis=0)
-        return X_train,Y_train,X_test,Y_test
-
-
-    def gradient_descent(self, passes):
+    def gradient_descent(self):
         ''' 
-            Learn the parameters of the 2-layer ff nn.
+            Learn the network parameters
         '''
+        # initialize weights randomly
+        learn = 0.01 
+        alpha = 0.05
+        convergence = False 
+        epoch = 1
+        W1 = np.random.rand(1,self.k)
+        b1 = np.random.rand(self.k,1)
+        W2 = np.random.rand(self.k,1)
+        b2 = np.random.rand(1,1)
+        while not convergence:
+            print("epoch: ", epoch)
+            for x,y in zip(self.X,self.Y):
+                v1 = self.f_layer(W1,b1,x) # k x 1 hidden layer
+                v2 = self.f_layer(W2,b2,v1) # 1 x 1 y output
+            # update weight parameters - TODO!!!!
+            W1_change = learn * 1
+            b1_change = learn * 1
+            W2_change = learn * 1
+            b2_change = learn * 1
+            if W1_change < alpha and b1_change < alpha and W2_change < alpha and b2_change < alpha:
+                convergence = True
+            else:
+                W1 = np.subtract(W1,W1_change)
+                b1 = np.subtract(b1,b1_change)
+                W2 = np.subtract(W2,w2_change)
+                b2 = np.subtract(b2,b2_change)
+            epoch += 1
+        return W1,b1,W2,b2
 
-
-    # where k is the size of the immediate layer
-    def evaluate(self, x_data, y_data, k):
-        ''' 
-            Evaluate the current layer of the Network
+    def f_layer(self,W,b,v):
         '''
+            x to k x 1 hidden layer, then k x 1 hidden layer to y
+        '''
+        param = np.add(np.matmul(W.T,v),b)
+        return self.activation(param)
 
-        #initialize weight parameters randomly
-        self.w1 = random.uniform(-1,1)
-        self.w2 = random.uniform(-1,1)
-        self.b1 = random.uniform(-1,1)
-        self.b2 = random.uniform(-1,1) 
+    def activation(self,param):
+        '''
+            Activation function
+        '''
+        return 1/(1+np.exp(-param))
 
+    def evaluate(self):
+        '''
+            Plot network output along with given Y values for each input value X
+        '''
+        y_hats = []
+        for x,y in zip(self.X,self.Y):
+            v1 = f_layer(W1,b1,x)
+            v2 = f_layer(W2,b2,v1)
+            y_hats.append(v2)
 
-        # reaches congergence when the difference between the previous and 
-        # the current loss is small
-        # change loop to run until convergence. 
-        # for now just leave on set epochs so we can observe
-        epochs = 4
-        for x in range(epochs):
-            for idx in range(len(x_data)):
-                # compute network output
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111)
+        ax1.scatter(self.X,y_hats,c='r',label='Network output')
+        ax1.scatter(self.X,self.Y,c='b',label='Given Y Values')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.title('Network output and given Y values vs. X')
+        plt.legend(loc='lower left')
+        plt.savefig('problem6.png')
+        plt.show()
 
-
-
-
-                # calculate loss
-                x_1 = X.dot(self.w1) + self.b1
-                # apply activation function
-                a_1 = 1 / (1 + np.exp(-x_1))
-
-
-
-
-
+if __name__ == "__main__":
+    nn = NeuralNet_2L(data_file='hw2data_2.mat',k=5)
